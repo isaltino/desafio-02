8087c35bb2280f065fe4f23a305838698999b125
Mizael Isaltino <Mizael.isaltino@gmail.com>
2020-04-21 23:02:00 -0300

    Terminando o desafio

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,70 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
+  const { title, url, techs } = request.body;
+  const repositorie = { id: uuid(), title, url, techs, likes: 0 };
+  repositories.push(repositorie);
+  response.json(repositorie);
   // TODO
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const respositorieIndex = repositories.findIndex(
+    (respositorie) => respositorie.id === id
+  );
+
+  if (respositorieIndex < 0) {
+    return response.status(400).json({ erro: "ID not found." });
+  }
+
+  const { title, url, techs } = request.body;
+
+  const respositorie = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[respositorieIndex].likes,
+  };
+
+  repositories[respositorieIndex] = respositorie;
+
+  response.json(respositorie);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const respositorieIndex = repositories.findIndex(
+    (respositorie) => respositorie.id === id
+  );
+
+  if (respositorieIndex < 0) {
+    return response.status(400).json({ erro: "ID not found." });
+  }
+
+  repositories.splice(respositorieIndex, 1);
+
+  response.status(204).json([]);
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const respositorieIndex = repositories.findIndex(
+    (respositorie) => respositorie.id === id
+  );
+
+  if (respositorieIndex < 0) {
+    return response.status(400).json({ erro: "ID not found." });
+  }
+
+  repositories[respositorieIndex].likes++;
+
+  response.json(repositories[respositorieIndex]);
 });
 
 module.exports = app;
